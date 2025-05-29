6bcc6b9f7696b9fb2897dc1c5e6493264a7069ff
helderlim <helderfelima16@gmail.com>
2025-05-29 20:42:37 -0300

    feat: finalizando a configuracao do prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    @Inject()
+    private readonly prisma: PrismaService;
+
+    async user(
+        UserWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    ) : Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: UserWhereUniqueInput,
+        });
+    }
+
+    async createUser(data: Prisma.UserCreateInput) {
+        return this.prisma.user.create({ data });
+    }
+
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+    }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({ where, data });
+    }
+
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({ where });
+    }
+}
